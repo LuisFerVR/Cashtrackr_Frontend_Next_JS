f273f6b7b5958056ac0005377e7b956675aa873a
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-04-23 10:54:35 -0600

    Creando el server component auth register y el client component de RegisterForm.

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,18 @@
+import RegisterForm from "@/components/auth/RegisterForm";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Cashtrackr - Registro",
+    description: "Crea una cuenta para controlar tus finanzas",
+    keywords: "registro, finanzas, cashtrackr, nextjs, tailwindcss",
+}
+
+export default function RegisterPage() {
+    return (
+    <>
+        <h1 className="font-black text-6xl text-purple-950">Crear una cuenta</h1>
+        <p className="text-3xl font-bold">y controla tus <span className="text-amber-500">finanzas</span></p>
+        <RegisterForm />
+    </>
+  )
+}
