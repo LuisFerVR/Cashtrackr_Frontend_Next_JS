dfb3c783e8c137392f859c55c54cc8c319956e96
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-05-08 16:05:09 -0600

    Se instaló react-progressbar-circle y se creó el componente ProgressBar que recibé la cantidad (value) que mostrará la gráfica.  Se personalizí en el mismo componente la gráfica.

--- app/admin/budgets/[id]/page.tsx
+++ app/admin/budgets/[id]/page.tsx
@@ -1,5 +1,7 @@
+import ProgressBar from "@/components/budgets/ProgressBar";
 import AddExpenseButton from "@/components/expenses/AddExpenseButton";
 import ExpenseMenu from "@/components/expenses/ExpensesMenu";
+import Amount from "@/components/UI/Amount";
 import ModalContainer from "@/components/UI/ModalContainer";
 import { getBudgetById } from "@/src/services/budgets";
 import { formatCurrency, formatDate } from "@/src/utils";
@@ -22,7 +24,14 @@ export default async function BudgetDetailsPage({
 }: {
   params: { id: string };
 }) {
+
   const budget = await getBudgetById(params.id);
+  const totalSpent = budget.expenses.reduce((total, expense) => {
+    return +expense.amount + total
+  },0)
+  const totalAvailable = +budget.amount - totalSpent;
+  const percentage = +((totalSpent / +budget.amount) * 100).toFixed(2);
+
   return (
     <>
       <div className="flex justify-between items-center">
@@ -37,6 +46,25 @@ export default async function BudgetDetailsPage({
 
       {budget.expenses.length ? (
         <>
+          <div className="grid grid-cols-1 md:grid-cols-2 mt-10">
+            <div>
+              <ProgressBar percentage={percentage}/>
+            </div>
+            <div className="flex flex-col gap-y-5 justify-center items-center md:items-start gap-5">
+              <Amount 
+                label="Presupuesto"
+                amount={+budget.amount}
+              />
+              <Amount 
+                label="Disponible"
+                amount={totalAvailable}
+              />
+              <Amount 
+                label="Gastado"
+                amount={+totalSpent}
+              />
+            </div>
+          </div>
           <h1 className="font-blac text-4xl text-purple-950 mt-10">
             Gastos en este presupuesto
           </h1>
